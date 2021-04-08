7e3473f70bebaa6e0c435262a4865850e0f39f9e
MartinezJB <martinez.joaquin.b@gmail.com>
2021-04-07 23:47:06 -0300

    ya, solo faltan los testgit push origin master

--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
@@ -1,15 +1,15 @@
 package ar.edu.unahur.obj2.vendedores
 
-class CentroDistribucion() {
+class CentroDistribucion(val vendedores: List<Vendedor>) {
 
-    val vendedores = mutableListOf<Vendedor>()
+    //val vendedores = mutableListOf<Vendedor>()
 
-    fun vendedorEstrella() = vendedores.maxBy { it.puntajeCertificaciones() }
+    fun vendedorEstrella() = this.vendedores.maxBy { it.puntajeCertificaciones() }
 
-    fun puedeCubrirCiudad(ciudad: Ciudad) = vendedores.any { it.puedeTrabajarEn(ciudad) }
+    fun puedeCubrirCiudad(ciudad: Ciudad) = this.vendedores.any { it.puedeTrabajarEn(ciudad) }
 
     //"esGenerico()" lo implemente en la clase vendedor
-    fun vendedoresGenericos() = vendedores.filter { it.esGenerico() }
+    fun vendedoresGenericos() = this.vendedores.filter { it.esGenerico() }
 
-    fun esRobusto() = vendedores.filter { it.esFirme() }.size >= 3
+    fun esRobusto() = this.vendedores.filter { it.esFirme() }.size >= 3
 }
